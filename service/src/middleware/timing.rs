d9cd543519abd3620fd260d982dc831bcc9987e8
tangxiangong <tangxiangong@gmail.com>
2025-01-21 10:57:12 +0800

    feat: 添加响应耗时中间件，响应头中包含 `X-Response-Elapsed`

---
+++ service/src/middleware/timing.rs
@@ -0,0 +1,17 @@
+use axum::{extract::Request, http::HeaderValue, middleware::Next, response::Response};
+use std::time::Instant;
+
+/// 计算响应耗时的中间件
+/// 会在响应头中添加 `X-Response-Elapsed` 字段
+pub async fn timing(req: Request, next: Next) -> Response {
+    let start = Instant::now();
+    let mut res = next.run(req).await;
+    let elapsed = start.elapsed().as_millis();
+    let headermap = res.headers_mut();
+    if let Ok(v) = HeaderValue::from_str(&format!("{}ms", elapsed)) {
+        headermap.insert("X-Response-Elapsed", v);
+        res
+    } else {
+        res
+    }
+}
