095bb89bc84a933bba55b4a9afb5a69014206bf0
tangxiangong <tangxiangong@gmail.com>
2025-01-21 10:56:36 +0800

    feat: 添加全局中间件 `trace`, `cors`, `timing`, 和 `timeout` ，配置依赖项

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use axum::http::Method;
+use axum::{routing::get, Router};
+use sea_orm::DbConn;
+use service::middleware::timing;
+use std::time::Duration;
+use tower::ServiceBuilder;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    timeout::TimeoutLayer,
+    trace::TraceLayer,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type StateRouter = Router<DbConn>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn compose() -> StateRouter {
+    Router::new()
+        .route("/api", get(|| async { "Hello, World!" }))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(
+                    CorsLayer::new()
+                        .allow_methods([Method::GET, Method::POST])
+                        .allow_origin(Any),
+                )
+                .layer(axum::middleware::from_fn(timing))
+                .layer(TimeoutLayer::new(Duration::from_secs(10))),
+        )
 }
