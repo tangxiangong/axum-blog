ff88f2bcbbe4c9cd37465e3bdc8dc42fa4f9667e
tangxiangong <tangxiangong@gmail.com>
2025-01-22 20:11:20 +0800

    feat: 更新登录接口，支持记住我功能并添加 JWT 与 Session 认证中间件

--- service/src/handler/login.rs
+++ service/src/handler/login.rs
@@ -1,15 +1,17 @@
-use axum::{extract::State, response::IntoResponse};
+use axum::{extract::State, http::HeaderValue, response::IntoResponse};
 use common::{
-    model::{AuthInfo, Claims, Login, LoginQuery},
+    model::{Claims, Login, RememberMe},
     AppError, AppResponse,
 };
 use database::admin::get_password;
 use sea_orm::DbConn;
 
+// TODO 利用 Redis 实现 Session 机制
+
 /// 登录接口 API: `/login`
 /// 默认不会返回 JWT, 除非用户选择了`记住我`，对应于 `/login?remember_me=true`
 pub async fn signin(
-    payload: LoginQuery,
+    payload: RememberMe,
     State(db_conn): State<DbConn>,
     admin: Login,
 ) -> impl IntoResponse {
@@ -17,11 +19,15 @@ pub async fn signin(
     if admin.password.ne(&exact_password) {
         return Err(AppError::unauth("密码错误"));
     }
+    let mut res = AppResponse::ok().into_response();
     if payload.remember_me {
         let claims = Claims::new(&admin.username);
-        let auth = claims.encode()?;
-        Ok(AppResponse::<AuthInfo, ()>::data(auth))
+        let token = claims.encode()?;
+        // let bearer = format!("Bearer {}", auth.token);
+        let token = HeaderValue::from_str(&token).map_err(|e| AppError::internal(e.to_string()))?;
+        res.headers_mut().insert("Bearer", token);
+        Ok(res)
     } else {
-        Ok(AppResponse::ok())
+        Ok(res)
     }
 }
--- service/src/middleware/auth.rs
+++ service/src/middleware/auth.rs
@@ -1,7 +1,9 @@
 use axum::{extract::Request, middleware::Next, response::Response};
 use common::model::Claims;
 
-/// JWT 认证中间件
-pub async fn auth(_claims: Claims, req: Request, next: Next) -> Response {
+/// JWT 与 Session 认证中间件
+/// TODO 利用 Redis 实现 Session 机制 Session 和 Claims 均实现 `OptionalFromRequestParts`
+pub async fn auth(_claims: Option<Claims>, req: Request, next: Next) -> Response {
+    // req.extensions_mut().insert(_claims.unwrap().username);
     next.run(req).await
 }
