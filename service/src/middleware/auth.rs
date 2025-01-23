1f72a61c8ac0bc66edc4c5c6ef21bb80b417740d
tangxiangong <tangxiangong@gmail.com>
2025-01-23 22:49:51 +0800

    feat: 更新认证中间件，增强会话管理和 JWT 验证逻辑

--- service/src/middleware/auth.rs
+++ service/src/middleware/auth.rs
@@ -1,22 +1,30 @@
-use axum::{extract::Request, middleware::Next};
+use axum::{extract::Request, middleware::Next, response::Response};
 use common::{
     model::{Claims, RedisConn, Session},
     AppError, AppResult,
 };
+use redis::AsyncCommands;
 
 /// JWT 与 Session 认证中间件
 pub async fn auth(
     claims: Option<Claims>,
     session: Option<Session>,
-    RedisConn(conn): RedisConn,
+    RedisConn(mut conn): RedisConn,
     req: Request,
     next: Next,
-) -> AppResult {
+) -> AppResult<Response> {
     // req.extensions_mut().insert(_claims.unwrap().username);
     if claims.is_none() && session.is_none() {
-        return Err(AppError::unauth("请登录"));
+        return Err(AppError::unauth("认证失败, 请重新登录"));
     }
-    session.unwrap().update(conn).await?;
-    next.run(req).await;
-    Ok(())
+    if session.is_some() {
+        session.unwrap().update(conn).await?;
+    } else {
+        // claims == Some
+        let token = claims.unwrap().encode()?;
+        if conn.exists(&token).await? {
+            return Err(AppError::unauth("认证失败, 请重新登录"));
+        }
+    }
+    Ok(next.run(req).await)
 }
