9866e8bcda283341b0e42aca0532adba5dd44d00
tangxiangong <tangxiangong@gmail.com>
2025-01-25 20:40:04 +0800

    feat: 限制请求体最大为 10 MB

--- service/src/middleware/global.rs
+++ service/src/middleware/global.rs
@@ -1,5 +1,5 @@
 use crate::middleware::timing;
-use axum::{http::Method, Router};
+use axum::{extract::DefaultBodyLimit, http::Method, Router};
 use setting::AppState;
 use std::time::Duration;
 use tower_http::{
@@ -12,6 +12,8 @@ pub fn global() -> Router<AppState> {
     Router::new()
         .layer(TimeoutLayer::new(Duration::from_secs(10)))
         .layer(axum::middleware::from_fn(timing))
+        // 限制请求体最大为 10 MB
+        .layer(DefaultBodyLimit::max(1024 * 1024 * 10))
         .layer(
             CorsLayer::new()
                 .allow_methods([Method::GET, Method::POST])
