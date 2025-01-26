63f0201e37e8920edd6073828058ff4be8bb42d8
tangxiangong <tangxiangong@gmail.com>
2025-01-26 16:58:27 +0800

    factor: 重构 `admin` 相关接口所在子模块

--- api/src/protected.rs
+++ api/src/protected.rs
@@ -1,17 +1,12 @@
 use crate::StateRouter;
-use axum::{routing::get, Router};
+use axum::Router;
 use common::AppState;
-use service::{handler::signout, middleware::auth};
-
-mod test;
-use test::test;
+use service::middleware::auth;
 
 mod admin;
 
 pub fn routes(state: AppState) -> StateRouter {
-    let root = Router::new().route("/protected", get(|| async { "hello" }));
-    root.nest("/protected", test())
-        .route("/signout", get(signout))
+    Router::new()
         .merge(admin::routes())
         .layer(axum::middleware::from_fn_with_state(state.clone(), auth))
 }
--- api/src/protected/admin.rs
+++ api/src/protected/admin.rs
@@ -1,8 +1,14 @@
-use axum::{routing::get, Router};
-use service::handler::admin::{get_info, update_info};
+use axum::{
+    routing::{get, patch},
+    Router,
+};
+use service::handler::{admin::*, signout};
 
 use crate::StateRouter;
 
 pub fn routes() -> StateRouter {
-    Router::new().route("/admin", get(get_info).patch(update_info))
+    Router::new()
+        .route("/signout", get(signout))
+        .route("/admin", get(get_info).patch(update_info))
+        .route("/admin/upload", patch(update_avatar))
 }
