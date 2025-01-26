fe286ddf38e312678b339cf893ca8f2f396681ee
tangxiangong <tangxiangong@gmail.com>
2025-01-26 13:41:55 +0800

    feat: rename `usr` to `admin` ，包含获取、更新信息和更新头像功能，同时删除 usr.rs 文件

---
+++ service/src/handler/admin.rs
@@ -0,0 +1,35 @@
+use super::utils;
+use axum::Extension;
+use common::{
+    entity::Admin,
+    model::{Image, UpdateAdminInfo},
+    AppResponse, AppResponseResult, AppResult, MySQLConn,
+};
+use database::admin as db;
+
+pub async fn get_info(
+    MySQLConn(db_conn): MySQLConn,
+    Extension(uid): Extension<String>,
+) -> AppResponseResult<Admin> {
+    let admin = db::get_info(&uid, &db_conn).await?;
+    Ok(AppResponse::data(admin))
+}
+
+pub async fn update_info(
+    MySQLConn(db_conn): MySQLConn,
+    Extension(uid): Extension<String>,
+    info: UpdateAdminInfo,
+) -> AppResult<()> {
+    db::update_info(&uid, info, &db_conn).await?;
+    Ok(())
+}
+
+pub async fn update_avatar(
+    MySQLConn(db_conn): MySQLConn,
+    Extension(uid): Extension<String>,
+    avatar: Image,
+) -> AppResult<()> {
+    let path = utils::save_image(avatar).await?;
+    db::update_avatar(&uid, &path, &db_conn).await?;
+    Ok(())
+}
