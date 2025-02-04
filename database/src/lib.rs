b81aa2cf4b6858d83a0555202437cf0b09c85101
tangxiangong <tangxiangong@gmail.com>
2025-02-04 18:06:25 +0800

    feat: Add category and tag database operations

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod admin;
 mod init;
 pub use init::init;
+pub mod category;
 pub mod jwt;
 mod macros;
+pub mod tag;
 pub mod website;
---
+++ database/src/tag.rs
@@ -0,0 +1,67 @@
+use common::{
+    AppError, AppResult,
+    entity::{ActiveTag, Tag, TagEntity, tag},
+};
+use sea_orm::{Set, entity::prelude::*};
+
+pub async fn add(tag_name: &str, db_conn: &DbConn) -> AppResult {
+    if find_by_name(tag_name, db_conn).await?.is_some() {
+        return Err(AppError::bad_request("标签已存在"));
+    }
+    let _ = ActiveTag {
+        name: Set(tag_name.to_owned()),
+        ..Default::default()
+    }
+    .insert(db_conn)
+    .await?;
+    Ok(())
+}
+
+pub async fn find_by_id(id: u32, db_conn: &DbConn) -> AppResult<Option<Tag>> {
+    let result = TagEntity::find_by_id(id).one(db_conn).await?;
+    Ok(result)
+}
+
+pub async fn update(tag_id: u32, new_tag_name: &str, db_conn: &DbConn) -> AppResult {
+    if find_by_id(tag_id, db_conn).await?.is_none() {
+        return Err(AppError::bad_request("标签不存在"));
+    }
+    if find_by_name(new_tag_name, db_conn).await?.is_some() {
+        return Err(AppError::bad_request("标签已存在"));
+    }
+
+    let mut active_model: ActiveTag = TagEntity::find_by_id(tag_id)
+        .one(db_conn)
+        .await?
+        .unwrap()
+        .into();
+
+    active_model.name = Set(new_tag_name.to_owned());
+    active_model.update(db_conn).await?;
+    Ok(())
+}
+
+pub async fn find_by_name(name: &str, db_conn: &DbConn) -> AppResult<Option<Tag>> {
+    let result = TagEntity::find()
+        .filter(tag::Column::Name.eq(name))
+        .one(db_conn)
+        .await?;
+    Ok(result)
+}
+
+pub async fn delete_by_id(id: u32, db_conn: &DbConn) -> AppResult {
+    if find_by_id(id, db_conn).await?.is_none() {
+        return Err(AppError::bad_request("标签不存在"));
+    }
+    let _ = TagEntity::delete_by_id(id).exec(db_conn).await?;
+    Ok(())
+}
+
+pub async fn delete_by_name(name: &str, db_conn: &DbConn) -> AppResult {
+    let active_model: ActiveTag = match find_by_name(name, db_conn).await? {
+        Some(model) => model.into(),
+        None => return Err(AppError::bad_request("标签不存在")),
+    };
+    let _ = active_model.delete(db_conn).await?;
+    Ok(())
+}
