1505ebc9b505ee3e95d7b8b2051d773f60614e41
tangxiangong <tangxiangong@gmail.com>
2025-02-28 19:44:51 +0800

    move deepseek to a standalone crate

--- crates/deepseek/src/prelude.rs
+++
@@ -1,5 +0,0 @@
-pub use super::chat::{Chat, ChatBuilder, Message, Model, Role};
-pub use super::cli::DeepSeek;
-pub use super::error::Error;
-pub use super::response::Response;
-pub use super::stream::*;
